db1a092618dd6fa91c50b23dc143a6db40c80b5f
SergeyVodchits <sergey_vodchits@mail.ru>
2017-04-04 11:20:57 +0300

    task 7

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-/**
- * Created by Sila on 04.04.2017.
- */
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+app.get("", function(req, res){
+    res.send("adasdas");
+})
+
+app.listen(3228);
